f52a85858630795eea4ce3b450a66bd38f6162bd
hss01248 <hss012489@gmail.com>
2020-05-29 11:18:05 +0800

    Revert "nothing"
    This reverts commit 6dc3cd0b

--- app/src/main/java/com/hss01248/beanvalidatordemo/MyBean.java
+++ app/src/main/java/com/hss01248/beanvalidatordemo/MyBean.java
@@ -1,7 +1,5 @@
 package com.hss01248.beanvalidatordemo;
 
-import com.hss01248.beanvalidator.NeedValidate;
-
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.validation.constraints.Size;
@@ -11,7 +9,6 @@ import javax.validation.constraints.Size;
  * data:2020-05-18
  * desc:
  */
-@NeedValidate
 public class MyBean {
     @NotEmpty(message = "error_empty")
     @Size(min = 1, max = 16,message = "not_fit_length")
