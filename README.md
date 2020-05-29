6dc3cd0b33e3f04b6f883583db36698b4ee2e164
hss01248 <hss012489@gmail.com>
2020-05-29 11:17:43 +0800

    nothing

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Hibernate Validator on Android
 
-## purpose
+## 场景
 
 拿到后台数据后,如何更高效地校验数据有效性,而不是在代码里写一堆if else判断?
 
@@ -10,8 +10,6 @@ Bean Validation 2.0（JSR 380）定义了用于实体和方法验证的元数据
 
 https://stackoverflow.com/questions/29315472/android-and-hibernate-validator-it-is-possible-to-use-together
 
-http://hibernate.org/validator/documentation/
-
 
 
 
@@ -20,7 +18,7 @@ http://hibernate.org/validator/documentation/
 
 
 
-![image-20200518152652333](image-20200518152652333.png)
+![image-20200518152652333](/Users/hss/aku/BeanValidatordemo/image-20200518152652333.png)
 
 
 
@@ -32,19 +30,23 @@ BeanValidator.init(this);
 
 ## 使用
 
-![image-20200518152741233](image-20200518152741233.png)
+```
+@NeedValidate加到类上,不加代表不开启bean校验
+```
+
+![image-20200518152741233](/Users/hss/aku/BeanValidatordemo/image-20200518152741233.png)
 
 ## 提供了中英文的默认文案:
 
-![image-20200518153928700](image-20200518153928700.png)
+![image-20200518153928700](/Users/hss/aku/BeanValidatordemo/image-20200518153928700.png)
 
-![image-20200518152831874](image-20200518152831874.png)
+![image-20200518152831874](/Users/hss/aku/BeanValidatordemo/image-20200518152831874.png)
 
 
 
 ## 自定义配置文案:
 
-![image-20200518152852237](image-20200518152852237.png)
+![image-20200518152852237](/Users/hss/aku/BeanValidatordemo/image-20200518152852237.png)
 
 
 
@@ -60,6 +62,19 @@ if(!TextUtils.isEmpty(errorMsg)){
     //拿到合格的bean
 }
 ```
+
+## 在retrofit中的使用:
+
+```
+Retrofit retrofit = new Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactoryWithBeanValidator.create())
+```
+
+
+
+# gradle 引用:
+
+
 
 # 注意
 
@@ -94,64 +109,5 @@ andResGuard {
 
 应该放到网络框架json解析的下一步,不要放到主线程
 
-![image-20200518153102618](image-20200518153102618.png)
-
-
-
-## hibernate的校验模式
-
-上面例子中一次性返回了所有验证不通过的集合，通常按顺序验证到第一个字段不符合验证要求时，就可以直接拒绝请求了。Hibernate Validator有以下两种验证模式：
-
-
-
-### 1、普通模式（默认是这个模式）
-
-　　普通模式(会校验完所有的属性，然后返回所有的验证失败信息)
-
-
-
-### 2、快速失败返回模式
-
-　　快速失败返回模式(只要有一个验证失败，则返回)
-
-两种验证模式配置方式：（[参考官方文档](https://docs.jboss.org/hibernate/stable/validator/reference/en-US/html_single/#section-provider-specific-settings)）
-
-failFast：true 快速失败返回模式  false 普通模式 
-
-```
-.addProperty( "hibernate.validator.fail_fast", isDebugMode() ? "false": "true"  )
-```
-
-
-
-![image-20200518160247483](image-20200518160247483.png)
-
-![image-20200518160307881](image-20200518160307881.png)
-
-# 兼容性
-
-## 首先配置
-
-```
-.ignoreXmlConfiguration()
-```
-
-## 在低版本(api16,19)上报找不到Log_$logger;
-
-```
-IllegalArgumentException: Invalid logger interface org.hibernate.validator.internal.util.logging.Log (implementation not found)
-```
-
-解决方法: 将Log_$logger拷出来,包路径不变,类名改成常规名字即可:
-
-![image-20200518180117670](image-20200518180117670.png)
-
-注意要编译成功,还需要依赖:
-
-```
-implementation group: 'javax.xml', name: 'jaxb-api', version: '2.1'
-implementation group: 'javax.annotation', name: 'javax.annotation-api', version: '1.3.1'
-```
-
-## 至此,可兼容到api16
+![image-20200518153102618](/Users/hss/aku/BeanValidatordemo/image-20200518153102618.png)
 
---
+++ app/src/main/java/com/hss01248/beanvalidatordemo/Repo.java
@@ -0,0 +1,226 @@
+package com.hss01248.beanvalidatordemo;
+
+import com.google.gson.annotations.SerializedName;
+import com.hss01248.beanvalidator.NeedValidate;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * by hss
+ * data:2020/5/29
+ * desc:
+ */
+@NeedValidate
+public class Repo {
+
+    /**
+     * id : 82249083
+     * node_id : MDEwOlJlcG9zaXRvcnk4MjI0OTA4Mw==
+     * name : AdvancedLuban
+     * full_name : hss01248/AdvancedLuban
+     * private : false
+     * owner : {"login":"hss01248","id":10666343,"node_id":"MDQ6VXNlcjEwNjY2MzQz","avatar_url":"https://avatars2.githubusercontent.com/u/10666343?v=4","gravatar_id":"","url":"https://api.github.com/users/hss01248","html_url":"https://github.com/hss01248","followers_url":"https://api.github.com/users/hss01248/followers","following_url":"https://api.github.com/users/hss01248/following{/other_user}","gists_url":"https://api.github.com/users/hss01248/gists{/gist_id}","starred_url":"https://api.github.com/users/hss01248/starred{/owner}{/repo}","subscriptions_url":"https://api.github.com/users/hss01248/subscriptions","organizations_url":"https://api.github.com/users/hss01248/orgs","repos_url":"https://api.github.com/users/hss01248/repos","events_url":"https://api.github.com/users/hss01248/events{/privacy}","received_events_url":"https://api.github.com/users/hss01248/received_events","type":"User","site_admin":false}
+     * html_url : https://github.com/hss01248/AdvancedLuban
+     * description : An Advanced Compress Image Library for Android / 高效、简洁的图片压缩工具库
+     * fork : true
+     * url : https://api.github.com/repos/hss01248/AdvancedLuban
+     * forks_url : https://api.github.com/repos/hss01248/AdvancedLuban/forks
+     * keys_url : https://api.github.com/repos/hss01248/AdvancedLuban/keys{/key_id}
+     * collaborators_url : https://api.github.com/repos/hss01248/AdvancedLuban/collaborators{/collaborator}
+     * teams_url : https://api.github.com/repos/hss01248/AdvancedLuban/teams
+     * hooks_url : https://api.github.com/repos/hss01248/AdvancedLuban/hooks
+     * issue_events_url : https://api.github.com/repos/hss01248/AdvancedLuban/issues/events{/number}
+     * events_url : https://api.github.com/repos/hss01248/AdvancedLuban/events
+     * assignees_url : https://api.github.com/repos/hss01248/AdvancedLuban/assignees{/user}
+     * branches_url : https://api.github.com/repos/hss01248/AdvancedLuban/branches{/branch}
+     * tags_url : https://api.github.com/repos/hss01248/AdvancedLuban/tags
+     * blobs_url : https://api.github.com/repos/hss01248/AdvancedLuban/git/blobs{/sha}
+     * git_tags_url : https://api.github.com/repos/hss01248/AdvancedLuban/git/tags{/sha}
+     * git_refs_url : https://api.github.com/repos/hss01248/AdvancedLuban/git/refs{/sha}
+     * trees_url : https://api.github.com/repos/hss01248/AdvancedLuban/git/trees{/sha}
+     * statuses_url : https://api.github.com/repos/hss01248/AdvancedLuban/statuses/{sha}
+     * languages_url : https://api.github.com/repos/hss01248/AdvancedLuban/languages
+     * stargazers_url : https://api.github.com/repos/hss01248/AdvancedLuban/stargazers
+     * contributors_url : https://api.github.com/repos/hss01248/AdvancedLuban/contributors
+     * subscribers_url : https://api.github.com/repos/hss01248/AdvancedLuban/subscribers
+     * subscription_url : https://api.github.com/repos/hss01248/AdvancedLuban/subscription
+     * commits_url : https://api.github.com/repos/hss01248/AdvancedLuban/commits{/sha}
+     * git_commits_url : https://api.github.com/repos/hss01248/AdvancedLuban/git/commits{/sha}
+     * comments_url : https://api.github.com/repos/hss01248/AdvancedLuban/comments{/number}
+     * issue_comment_url : https://api.github.com/repos/hss01248/AdvancedLuban/issues/comments{/number}
+     * contents_url : https://api.github.com/repos/hss01248/AdvancedLuban/contents/{+path}
+     * compare_url : https://api.github.com/repos/hss01248/AdvancedLuban/compare/{base}...{head}
+     * merges_url : https://api.github.com/repos/hss01248/AdvancedLuban/merges
+     * archive_url : https://api.github.com/repos/hss01248/AdvancedLuban/{archive_format}{/ref}
+     * downloads_url : https://api.github.com/repos/hss01248/AdvancedLuban/downloads
+     * issues_url : https://api.github.com/repos/hss01248/AdvancedLuban/issues{/number}
+     * pulls_url : https://api.github.com/repos/hss01248/AdvancedLuban/pulls{/number}
+     * milestones_url : https://api.github.com/repos/hss01248/AdvancedLuban/milestones{/number}
+     * notifications_url : https://api.github.com/repos/hss01248/AdvancedLuban/notifications{?since,all,participating}
+     * labels_url : https://api.github.com/repos/hss01248/AdvancedLuban/labels{/name}
+     * releases_url : https://api.github.com/repos/hss01248/AdvancedLuban/releases{/id}
+     * deployments_url : https://api.github.com/repos/hss01248/AdvancedLuban/deployments
+     * created_at : 2017-02-17T02:37:47Z
+     * updated_at : 2019-08-16T09:13:05Z
+     * pushed_at : 2017-03-08T03:29:22Z
+     * git_url : git://github.com/hss01248/AdvancedLuban.git
+     * ssh_url : git@github.com:hss01248/AdvancedLuban.git
+     * clone_url : https://github.com/hss01248/AdvancedLuban.git
+     * svn_url : https://github.com/hss01248/AdvancedLuban
+     * homepage : http://shaohui.me
+     * size : 353
+     * stargazers_count : 4
+     * watchers_count : 4
+     * language : Java
+     * has_issues : false
+     * has_projects : true
+     * has_downloads : true
+     * has_wiki : true
+     * has_pages : false
+     * forks_count : 1
+     * mirror_url : null
+     * archived : false
+     * disabled : false
+     * open_issues_count : 0
+     * license : {"key":"apache-2.0","name":"Apache License 2.0","spdx_id":"Apache-2.0","url":"https://api.github.com/licenses/apache-2.0","node_id":"MDc6TGljZW5zZTI="}
+     * forks : 1
+     * open_issues : 0
+     * watchers : 4
+     * default_branch : master
+     */
+
+    public long id;
+    @NotNull
+    public String keyword;//增加一个不存在的字段,用作校验测试
+    public String node_id;
+    public String name;
+    public String full_name;
+    @SerializedName("private")
+    public boolean privateX;
+    public OwnerBean owner;
+    public String html_url;
+    public String description;
+    public boolean fork;
+    public String url;
+    public String forks_url;
+    public String keys_url;
+    public String collaborators_url;
+    public String teams_url;
+    public String hooks_url;
+    public String issue_events_url;
+    public String events_url;
+    public String assignees_url;
+    public String branches_url;
+    public String tags_url;
+    public String blobs_url;
+    public String git_tags_url;
+    public String git_refs_url;
+    public String trees_url;
+    public String statuses_url;
+    public String languages_url;
+    public String stargazers_url;
+    public String contributors_url;
+    public String subscribers_url;
+    public String subscription_url;
+    public String commits_url;
+    public String git_commits_url;
+    public String comments_url;
+    public String issue_comment_url;
+    public String contents_url;
+    public String compare_url;
+    public String merges_url;
+    public String archive_url;
+    public String downloads_url;
+    public String issues_url;
+    public String pulls_url;
+    public String milestones_url;
+    public String notifications_url;
+    public String labels_url;
+    public String releases_url;
+    public String deployments_url;
+    public String created_at;
+    public String updated_at;
+    public String pushed_at;
+    public String git_url;
+    public String ssh_url;
+    public String clone_url;
+    public String svn_url;
+    public String homepage;
+    public int size;
+    public int stargazers_count;
+    public int watchers_count;
+    public String language;
+    public boolean has_issues;
+    public boolean has_projects;
+    public boolean has_downloads;
+    public boolean has_wiki;
+    public boolean has_pages;
+    public int forks_count;
+    public Object mirror_url;
+    public boolean archived;
+    public boolean disabled;
+    public int open_issues_count;
+    public LicenseBean license;
+    public int forks;
+    public int open_issues;
+    public int watchers;
+    public String default_branch;
+
+    public static class OwnerBean {
+        /**
+         * login : hss01248
+         * id : 10666343
+         * node_id : MDQ6VXNlcjEwNjY2MzQz
+         * avatar_url : https://avatars2.githubusercontent.com/u/10666343?v=4
+         * gravatar_id :
+         * url : https://api.github.com/users/hss01248
+         * html_url : https://github.com/hss01248
+         * followers_url : https://api.github.com/users/hss01248/followers
+         * following_url : https://api.github.com/users/hss01248/following{/other_user}
+         * gists_url : https://api.github.com/users/hss01248/gists{/gist_id}
+         * starred_url : https://api.github.com/users/hss01248/starred{/owner}{/repo}
+         * subscriptions_url : https://api.github.com/users/hss01248/subscriptions
+         * organizations_url : https://api.github.com/users/hss01248/orgs
+         * repos_url : https://api.github.com/users/hss01248/repos
+         * events_url : https://api.github.com/users/hss01248/events{/privacy}
+         * received_events_url : https://api.github.com/users/hss01248/received_events
+         * type : User
+         * site_admin : false
+         */
+
+        public String login;
+        public long id;
+        public String node_id;
+        public String avatar_url;
+        public String gravatar_id;
+        public String url;
+        public String html_url;
+        public String followers_url;
+        public String following_url;
+        public String gists_url;
+        public String starred_url;
+        public String subscriptions_url;
+        public String organizations_url;
+        public String repos_url;
+        public String events_url;
+        public String received_events_url;
+        public String type;
+        public boolean site_admin;
+    }
+
+    public static class LicenseBean {
+        /**
+         * key : apache-2.0
+         * name : Apache License 2.0
+         * spdx_id : Apache-2.0
+         * url : https://api.github.com/licenses/apache-2.0
+         * node_id : MDc6TGljZW5zZTI=
+         */
+
+        public String key;
+        public String name;
+        public String spdx_id;
+        public String url;
+        public String node_id;
+    }
+}
